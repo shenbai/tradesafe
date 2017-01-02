c93b5b07421fc54cdb049a1ffa356ebf14e7a02c
zhaoziqiang3 <zhaoziqiang3@source.jd.com>
2017-01-02 19:49:25 +0800

    v0.2221

---
+++ org/tradesafe/bt/selector.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+__author__ = "tack"
+
+from org.tradesafe.data.history_data import HistoryData
+from datetime import datetime, timedelta
+import traceback
+import sys
+from math import *
+import talib
+
+
+class Selector(object):
+
+    def __init__(self):
+
+        pass
+
